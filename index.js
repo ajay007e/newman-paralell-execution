8a2265d9a821f9acf3adc81e0fbe3724383008ba
Ajay Mathew <ajay.mathew@batonsystems.com>
2023-12-17 12:55:49 +0530

    init : add cli prompt to accept user input

--- index.js
+++ index.js
@@ -2,14 +2,10 @@
 import * as util from './lib/util.js'
 
 
-
-
-
-
-
-
-const main = () => {
+const main = async () => {
     util.initializeFileSetups();
+    const result = await util.executePrompt();
+    !result?.status ? console.error(`Prompt execution failed`) : console.log(result?.response)
 }
 
 main()
