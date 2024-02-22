17d3c367387fe89a15014bbaee2ac678d6fa0fd9
Ajay Mathew <ajay.mathew@batonsystems.com>
2024-02-22 11:37:36 +0530

    fix : test failure summery not showing and minor code refactoring

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const main = async () => {
         util.initializeFileSetups();
         const resultFromPrompt = await util.executePrompt();
         const resultFromSetup = !resultFromPrompt?.status ? exitFails(`Prompt execution failed`) : await util.configureSetups(resultFromPrompt?.response);
-        const resultFromTest = !resultFromSetup?.status ? exitFails(`Configuring setup failed`) : await util.executeTests(resultFromSetup?.data);
-        const resultFromGenerateReport = !resultFromTest?.status ? exitFails(`Test execution failed`) : await util.generateReport(resultFromTest);
-        !resultFromGenerateReport?.status ? exitFails(`Report Generation failed`) : util.showGenerateReports();
+        !resultFromSetup?.status ? exitFails(`Configuring setup failed`) : await util.executeTests(resultFromSetup?.data);
     } catch (err){
         console.error(err)
     }
